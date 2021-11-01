62232df4970a67f70cbab62ff92791508978de03
AdrianNietoRamos <adrianietoemfb@gmail.com>
2021-11-01 19:28:16 +0000

    Update VerView.java

--- src/main/java/dad/iniciosesion/ver/VerView.java
+++ src/main/java/dad/iniciosesion/ver/VerView.java
@@ -4,14 +4,28 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class VerView extends VBox{
 private TextField usuarioText;
-private TextField contrasenaText;
+private PasswordField contrasenaText;
 private CheckBox usarCheck;
 private Button accederButton;
 private Button cancelarButton;
+
+public VerView() {
+	super();
+	usuarioText=new TextField();
+	contrasenaText=new PasswordField();
+	usarCheck=new CheckBox();
+	accederButton=new Button();
+	cancelarButton=new Button();
+	
+	HBox usuarioBox=new HBox(5, new Label("Usuario:"),usuarioText);
+	
+	
+}
 }
